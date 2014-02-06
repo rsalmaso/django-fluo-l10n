1a653d654a751d351bb91ed18c0da324b0cf1637
Raffaele Salmaso <raffaele@salmaso.org>
2014-02-06 09:29:19 +0100

    regenerate migrations

--- l10n/migrations/0001_initial.py
+++ l10n/migrations/0001_initial.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 
 # Copyright (C) 2007-2014, Raffaele Salmaso <raffaele@salmaso.org>
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
@@ -20,96 +20,96 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
+from south.utils import datetime_utils as datetime
 from south.db import db
-from fluo import models
-from l10n.models import *
+from south.v2 import SchemaMigration
+from django.db import models
 
-class Migration:
-    
+
+class Migration(SchemaMigration):
+
     def forwards(self, orm):
-        
-        # Adding model 'AdministrativeArea'
-        db.create_table('l10n_administrativearea', (
-            ('id', orm['l10n.AdministrativeArea:id']),
-            ('status', orm['l10n.AdministrativeArea:status']),
-            ('country', orm['l10n.AdministrativeArea:country']),
-            ('name', orm['l10n.AdministrativeArea:name']),
-            ('abbrev', orm['l10n.AdministrativeArea:abbrev']),
-        ))
-        db.send_create_signal('l10n', ['AdministrativeArea'])
-        
-        # Adding model 'CountryTranslation'
-        db.create_table('l10n_countrytranslation', (
-            ('id', orm['l10n.CountryTranslation:id']),
-            ('language', orm['l10n.CountryTranslation:language']),
-            ('country', orm['l10n.CountryTranslation:country']),
-            ('name', orm['l10n.CountryTranslation:name']),
-            ('printable_name', orm['l10n.CountryTranslation:printable_name']),
-        ))
-        db.send_create_signal('l10n', ['CountryTranslation'])
-        
         # Adding model 'Country'
-        db.create_table('l10n_country', (
-            ('id', orm['l10n.Country:id']),
-            ('status', orm['l10n.Country:status']),
-            ('name', orm['l10n.Country:name']),
-            ('printable_name', orm['l10n.Country:printable_name']),
-            ('iso2_code', orm['l10n.Country:iso2_code']),
-            ('iso3_code', orm['l10n.Country:iso3_code']),
-            ('numcode', orm['l10n.Country:numcode']),
-            ('continent', orm['l10n.Country:continent']),
-            ('admin_area', orm['l10n.Country:admin_area']),
+        db.create_table(u'l10n_country', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('status', self.gf('fluo.db.models.fields.StatusField')()),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=128, db_index=True)),
+            ('printable_name', self.gf('django.db.models.fields.CharField')(max_length=128, db_index=True)),
+            ('iso2_code', self.gf('django.db.models.fields.CharField')(unique=True, max_length=2, db_index=True)),
+            ('iso3_code', self.gf('django.db.models.fields.CharField')(unique=True, max_length=3)),
+            ('numcode', self.gf('django.db.models.fields.PositiveSmallIntegerField')(null=True, blank=True)),
+            ('continent', self.gf('django.db.models.fields.CharField')(max_length=13)),
+            ('admin_area', self.gf('django.db.models.fields.CharField')(db_index=True, max_length=20, null=True, blank=True)),
         ))
-        db.send_create_signal('l10n', ['Country'])
-        
-        # Creating unique_together for [language, country] on CountryTranslation.
-        db.create_unique('l10n_countrytranslation', ['language', 'country_id'])
-        
-    
-    
+        db.send_create_signal(u'l10n', ['Country'])
+
+        # Adding model 'CountryTranslation'
+        db.create_table(u'l10n_countrytranslation', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('language', self.gf('django.db.models.fields.CharField')(max_length=5, db_index=True)),
+            ('country', self.gf('django.db.models.fields.related.ForeignKey')(related_name='translations', to=orm['l10n.Country'])),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=128, db_index=True)),
+            ('printable_name', self.gf('django.db.models.fields.CharField')(max_length=128, db_index=True)),
+        ))
+        db.send_create_signal(u'l10n', ['CountryTranslation'])
+
+        # Adding unique constraint on 'CountryTranslation', fields ['language', 'country']
+        db.create_unique(u'l10n_countrytranslation', ['language', 'country_id'])
+
+        # Adding model 'AdministrativeArea'
+        db.create_table(u'l10n_administrativearea', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('status', self.gf('fluo.db.models.fields.StatusField')()),
+            ('country', self.gf('django.db.models.fields.related.ForeignKey')(related_name='administrative_areas', to=orm['l10n.Country'])),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=60, db_index=True)),
+            ('abbrev', self.gf('django.db.models.fields.CharField')(max_length=3, null=True, blank=True)),
+        ))
+        db.send_create_signal(u'l10n', ['AdministrativeArea'])
+
+
     def backwards(self, orm):
-        
-        # Deleting unique_together for [language, country] on CountryTranslation.
-        db.delete_unique('l10n_countrytranslation', ['language', 'country_id'])
-        
-        # Deleting model 'AdministrativeArea'
-        db.delete_table('l10n_administrativearea')
-        
-        # Deleting model 'CountryTranslation'
-        db.delete_table('l10n_countrytranslation')
-        
+        # Removing unique constraint on 'CountryTranslation', fields ['language', 'country']
+        db.delete_unique(u'l10n_countrytranslation', ['language', 'country_id'])
+
         # Deleting model 'Country'
-        db.delete_table('l10n_country')
-        
-    
-    
+        db.delete_table(u'l10n_country')
+
+        # Deleting model 'CountryTranslation'
+        db.delete_table(u'l10n_countrytranslation')
+
+        # Deleting model 'AdministrativeArea'
+        db.delete_table(u'l10n_administrativearea')
+
+
     models = {
-        'l10n.administrativearea': {
+        u'l10n.administrativearea': {
+            'Meta': {'ordering': "('name',)", 'object_name': 'AdministrativeArea'},
             'abbrev': ('django.db.models.fields.CharField', [], {'max_length': '3', 'null': 'True', 'blank': 'True'}),
-            'country': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'administrative_areas'", 'to': "orm['l10n.Country']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'country': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'administrative_areas'", 'to': u"orm['l10n.Country']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '60', 'db_index': 'True'}),
-            'status': ('models.StatusField', [], {})
+            'status': ('fluo.db.models.fields.StatusField', [], {})
         },
-        'l10n.country': {
+        u'l10n.country': {
+            'Meta': {'ordering': "('iso2_code', 'name')", 'object_name': 'Country'},
             'admin_area': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '20', 'null': 'True', 'blank': 'True'}),
             'continent': ('django.db.models.fields.CharField', [], {'max_length': '13'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'iso2_code': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '2', 'db_index': 'True'}),
             'iso3_code': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '3'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '128', 'db_index': 'True'}),
             'numcode': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
             'printable_name': ('django.db.models.fields.CharField', [], {'max_length': '128', 'db_index': 'True'}),
-            'status': ('models.StatusField', [], {})
+            'status': ('fluo.db.models.fields.StatusField', [], {})
         },
-        'l10n.countrytranslation': {
-            'Meta': {'unique_together': "(('language', 'country'),)"},
-            'country': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'translations'", 'to': "orm['l10n.Country']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+        u'l10n.countrytranslation': {
+            'Meta': {'unique_together': "(('language', 'country'),)", 'object_name': 'CountryTranslation'},
+            'country': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'translations'", 'to': u"orm['l10n.Country']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'language': ('django.db.models.fields.CharField', [], {'max_length': '5', 'db_index': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '128', 'db_index': 'True'}),
             'printable_name': ('django.db.models.fields.CharField', [], {'max_length': '128', 'db_index': 'True'})
         }
     }
-    
+
     complete_apps = ['l10n']
